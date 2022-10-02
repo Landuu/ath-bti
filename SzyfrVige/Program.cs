8c7f7e2fc59f3c92d10bf012773cf4336e7ba2b7
Bartłomiej Wiercigroch <bw057219@student.ath.edu.pl>
2022-10-02 23:59:09 +0200

    Removed cipher

--- SzyfrVige/Program.cs
+++ SzyfrVige/Program.cs
@@ -2,48 +2,6 @@
 
 namespace SztfrVig
 {
-    public class CipherEncoder
-    {
-        private readonly char[] _letters;
-        private readonly int _shift;
-
-        public CipherEncoder(int shift = 7)
-        {
-            string charset = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ";
-            _letters = charset.ToCharArray();
-            _shift = shift;
-        }
-
-        private char Shift(char ch, bool forward)
-        {
-            if (!char.IsLetter(ch)) return ch;
-
-            int charIndex = forward ? Array.IndexOf(_letters, ch) + _shift : Array.IndexOf(_letters, ch) - _shift;
-            if (forward)
-            {
-                if (charIndex >= _letters.Length) charIndex -= _letters.Length;
-            }
-            else
-            {
-                if (charIndex < 0) charIndex += _letters.Length;
-            }
-            return _letters[charIndex];
-        }
-
-        public string ShiftLoop(string message, bool forward)
-        {
-            message = message.Trim().ToUpper();
-            char[] messageChars = message.ToCharArray();
-            for (int i = 0; i < messageChars.Length; i++)
-                messageChars[i] = Shift(messageChars[i], forward);
-            return new string(messageChars);
-        }
-
-        public string Encode(string message) => ShiftLoop(message, true);
-        public string Decode(string message) => ShiftLoop(message, false);
-    }
-
-
     public class Vig
     {
         private readonly char[] _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();

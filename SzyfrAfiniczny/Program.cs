1de5007090958a428f8952011639a60f0f39be4b
Bartłomiej Wiercigroch <bw057219@student.ath.edu.pl>
2022-10-19 19:07:44 +0200

    Afiniczny

--- SzyfrAfiniczny/Program.cs
+++ SzyfrAfiniczny/Program.cs
@@ -1,67 +1,61 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-namespace SzyfrAfiniczny
+﻿public static class SzyfrAfiniczny
 {
-    public class SzyfrAfiniczny
+    private const string _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static string Encipher(string text, int a, int b)
     {
-        //private static readonly char[] _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-        private static readonly char[] _letters = "ABCDKLMNOT".ToCharArray();
-
-        public static bool IsPrime(int n)
+        if (!IsPrime(a)) throw new ArgumentException("a nie jest liczba pierwsza");
+        string output = string.Empty;
+        foreach (char c in text.ToUpper())
         {
-            if (n < 1) return false;
-            return Enumerable.Range(1, n).Where(x => n % x == 0)
-                                 .SequenceEqual(new[] { 1, n });
-        }
-
-        public static void Encipher(string text, (int a, int b) key)
-        {
-            if (!IsPrime(key.a) && key.a >= _letters.Length) return;
-
-            var chars = text.ToCharArray();
-            string output = "";
-            foreach(var c in chars)
+            if(!char.IsLetter(c))
             {
-                int charIdex = Array.IndexOf(_letters, c);
-                if (charIdex == -1) return;
-                int outputIndex = (key.a * charIdex + key.b) % _letters.Length;
-                output += _letters[outputIndex];
+                output += c;
+                continue;
             }
-            Console.WriteLine(output);
+            int shift = (a * _letters.IndexOf(c) + b) % _letters.Length;
+            output += _letters[shift];
         }
+        return output;
+    }
 
-        public static void Decipher(string text, (int a, int b) key)
+    public static string Decipher(string text, int a, int b)
+    {
+        if (!IsPrime(a)) throw new ArgumentException("a nie jest liczba pierwsza");
+        int inverse = Enumerable.Range(1, _letters.Length).First(i => i * a % _letters.Length == 1);
+        string output = string.Empty;
+        foreach (char c in text.ToUpper())
         {
-            if (!IsPrime(key.a) && key.a <= _letters.Length) return;
-
-            var chars = text.ToCharArray();
-            string output = "";
-            foreach (var c in chars)
+            if(!char.IsLetter(c))
             {
-                int charIdex = Array.IndexOf(_letters, c);
-                if (charIdex == -1) return;
-                int outputIndex = GetA(key.a) * (charIdex - key.b) % _letters.Length;
-                output += _letters[outputIndex % _letters.Length];
+                output += c;
+                continue;
             }
-            Console.WriteLine(output); 
+            int index = inverse * (_letters.IndexOf(c) - b) % _letters.Length;
+            int shift = index < 0 ? 26 + index : index;
+            output += _letters[shift];
         }
+        return output;
+    }
 
-        public static int GetA(int a)
-        {
-            for (int i = 1; i <= _letters.Length; i++)
-            {
-                if ((a * i % _letters.Length) == 1)
-                {
-                    return i;
-                }
-            }
-            return 0;
-        }
+    private static bool IsPrime(int a) =>
+       Enumerable.Range(1, Math.Min(a, _letters.Length)).Where(i => a % i == 0 && _letters.Length % i == 0).Count() == 1;
 
-        public static void Main()
-        {
-            SzyfrAfiniczny.Encipher("KOT", (3, 6));
-            SzyfrAfiniczny.Decipher("OAT", (3, 6));
-        }
+
+    public static void Main()
+    {
+        Console.Write("Podaj tekst:");
+        string? input = Console.ReadLine() ?? "";
+        Console.WriteLine("Podaj a:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Podaj b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        string cipher = Encipher(input, a, b);
+        string decipher = Decipher(cipher, a, b);
+        Console.WriteLine($" Input: {input}");
+        Console.WriteLine($" Cipher: {cipher}");
+        Console.WriteLine($" Decipher: {decipher}");
     }
 }
+
+
+
